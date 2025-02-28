2f38ff0bc84b59fc377bf4852d7a4fd0eba0ad68
rudyR1112 <rudolph.reid@ucf.edu>
2025-02-28 16:40:50 -0500

    Update Cis4615a .java

--- Cis4615a .java
+++ Cis4615a .java
@@ -1,23 +1,23 @@
 /******************************************************************************
- *  Compilation:  javac Cis4615a.java
- *  Execution:    java Cis4615a
+ *  Compilation:  javac Cis4340.java
+ *  Execution:    java Cis4340
  *
  *  Prints 
- *    "Hello, World - from the class of CIS4615 - Fall 2022 -UCF _YoUR NAME HERE_".
+ *    "Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here_)".
  *  By tradition, this is everyone's first program.
  *
  * Revision History:
- *   % java Cis4615a
- *  Hello, World - from the class of CIS4615 - Fall 2022 - UCF
+ *   % java Cis4340
+ *  Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)
  *  %
  *
  ******************************************************************************/
 
-public class Cis4615a {
+public class Cis4340 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World - from Rudolph Reid");
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)");
     }
 
 }
