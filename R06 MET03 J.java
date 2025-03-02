9b735546c0b080f4d351c2816024486246cbf51e
rudyR1112 <rudolph.reid@ucf.edu>
2025-03-01 22:36:31 -0500

    Create R06 MET03 J.java

---
+++ R06 MET03 J.java
@@ -0,0 +1,11 @@
+public void readSensitiveFile() {
+  try {
+    SecurityManager sm = System.getSecurityManager();
+    if (sm != null) {  // Check for permission to read file
+      sm.checkRead("/temp/tempFile");
+    }
+    // Access the file
+  } catch (SecurityException se) {
+    // Log exception
+  }
+}
