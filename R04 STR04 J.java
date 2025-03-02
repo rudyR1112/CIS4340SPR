ecaeea1b5d8ce9f20dadc2180fe129f24384e809
rudyR1112 <rudolph.reid@ucf.edu>
2025-03-01 22:38:11 -0500

    Create R04 STR04 J.java

---
+++ R04 STR04 J.java
@@ -0,0 +1,18 @@
+FileInputStream fis = null;
+try {
+  fis = new FileInputStream("SomeFile");
+  DataInputStream dis = new DataInputStream(fis);
+  byte[] data = new byte[1024];
+  dis.readFully(data);
+  String result = new String(data);
+} catch (IOException x) {
+  // Handle error
+} finally {
+  if (fis != null) {
+    try {
+      fis.close();
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
