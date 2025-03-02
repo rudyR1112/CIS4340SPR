55a47c7ef6a1760eb611729558d7e82a413e7068
rudyR1112 <rudolph.reid@ucf.edu>
2025-03-01 22:39:40 -0500

    Update R03 NUM08 J.java

--- R03 NUM08 J.java
+++ R03 NUM08 J.java
@@ -1,6 +1,6 @@
 double currentBalance; // User's cash balance
  
-void doDeposit(String userInput) {
+void doDeposit(String userInput){
   double val = 0;
   try {
     val = Double.valueOf(userInput);
@@ -8,9 +8,16 @@ void doDeposit(String userInput) {
     // Handle input format error
   }
  
+  if (Double.isInfinite(val)){
+    // Handle infinity error
+  }
+ 
+  if (Double.isNaN(val)) {
+    // Handle NaN error
+  }
+ 
   if (val >= Double.MAX_VALUE - currentBalance) {
     // Handle range error
   }
- 
   currentBalance += val;
 }
